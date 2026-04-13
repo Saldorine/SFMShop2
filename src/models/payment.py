3fac7b66613898f860b95789dd0f0072cbe926bc
Saldorin <gorbatenko999@gmail.com>
2026-04-13 13:38:35 +0500

    Итоговое задание 5 урока ООП. Создать миксины логирования, валидации, сериализации

--- src/models/payment.py
+++ src/models/payment.py
@@ -7,17 +7,23 @@ class Payment:
         raise NotImplementedError("Метод долен быть переопределен.")
 
 
-class CardPayment(Payment):
+class Loggable():
+    def log(self):
+        print("Loggable.log()")
+
+
+class CardPayment(Payment, Loggable):
     def __init__(self, amount, card_number):
         super().__init__(amount)
         self.__card_number = card_number
 
 
     def process_payment(self):
+        self.log()
         return f'Оплата картой **** {self.__card_number[-4:]}: {self.amount} руб.'
 
 
-class PayPalPayment(Payment):
+class PayPalPayment(Payment, Loggable):
     def __init__(self, amount, email):
         super().__init__(amount)
         self._email = email
@@ -28,4 +34,5 @@ class PayPalPayment(Payment):
 
 
     def process_payment(self):
+        self.log()
         return f'Оплата PayPal ({self._email}): {self.amount} руб.'
